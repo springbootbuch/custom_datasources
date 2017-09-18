97645b39edd7322f4b9d8490a366e13e3df11928
Michael J. Simons <michael@simons.ac>
2017-09-18 16:43:07 +0200

    Use 2.0.0.M4

--- src/main/java/de/springbootbuch/custom_datasources/SimpleBuilderUsageConfig.java
+++ src/main/java/de/springbootbuch/custom_datasources/SimpleBuilderUsageConfig.java
@@ -1,8 +1,8 @@
 package de.springbootbuch.custom_datasources;
 
 import javax.sql.DataSource;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
